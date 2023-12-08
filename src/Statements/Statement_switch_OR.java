82ac796f9c70c09f04d4f0a6b534cfb4b44f2fb4
Ashish Mishra <ashish.mishra.sse@gmail.com>
2023-12-08 11:03:37 +0530

    added saturday as WFH as per Reetika

--- src/Statements/Statement_switch_OR.java
+++ src/Statements/Statement_switch_OR.java
@@ -10,13 +10,14 @@ public class Statement_switch_OR
             case "Wednesday":
             case "Thursday":
             case "Friday":
+            case  "Saturday":
                 System.out.println("Its " + day + " --Lets do WFH");
                 break;
-            case  "Saturday":
+            case  "Sunday":
                 System.out.println("Its " + day + " --Lets dance");
                 break;
             default:
-                System.out.println("Its weekend....lets Party");
+                System.out.println("Please provide a valid day");
                 break;
 
         }
